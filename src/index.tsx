40bac644513c57c30939fdaa2489684a7c883ec6
Nikita <mestzatukovky@gmail.com>
2023-01-30 15:42:35 +0700

    Fixed word counter and transition to placeholder

--- src/index.tsx
+++ src/index.tsx
@@ -7,21 +7,23 @@ import "./index.css";
 function TextArea() {
 	const [currentContent, setCurrentContent] = createSignal("");
 	const [isFocused, setIsFocused] = createSignal(false);
+	// Acounts for all combination of whitespace chars and initial content length
+	const wordCount = () => currentContent().replace(/\s\s+/g, ' ').trimEnd().split(' ').length - Number(currentContent().length === 0);
 	
 	// Note(Nik4ant): This type anotation looks kinda horrible not gona lie
-	// TODO: proper onchange
-	function onTextareaChanged(e: Event & {currentTarget: HTMLTextAreaElement;target: Element;}) {
-		setCurrentContent(e.currentTarget.value)
+	// TODO: effect from original app
+	function onTextareaInput(e: InputEvent & {currentTarget: HTMLTextAreaElement; target: Element;}) {
+		setCurrentContent(e.currentTarget.value);
 	}
 
-	const placeholderElement = <p class="editor-placeholder fade-in-transition">Let your thoughts flow <i>mindlessly</i> like a river...</p> as HTMLParagraphElement;
+	const placeholderElement = <p class="editor-placeholder">Let your thoughts flow <i>mindlessly</i> like a river...</p> as HTMLParagraphElement;
+	// When placeholder is clicked it fades away and reveals editable textarea
 	placeholderElement.addEventListener("click", (_) => {
-		// Waiting for placeholder fade out animation
 		const initialAnimValue = placeholderElement.style.animation;
 		const fadeOutAnim = "fade-out 0.5s ease-out";
 
+		// Waiting for placeholder fade out animation
 		placeholderElement.addEventListener("animationend", (_) => {
-			console.debug("Hello?!");
 			placeholderElement.style.animation = initialAnimValue;
 			// Changing focus
 			setIsFocused(true);
@@ -30,11 +32,15 @@ function TextArea() {
 
 		placeholderElement.style.animation = fadeOutAnim;
 	});
-
-	const editableTextElement = <textarea rows="1" class="editor-text-field fade-in-transition" onchange={e => onTextareaChanged(e)}></textarea> as HTMLTextAreaElement;
-	// Changing focus to determine if placeholder needs to be displayed
+	// When focus from empty textarea is removed placeholder is displayed
+	const editableTextElement = <textarea rows="1" class="editor-text-field fade-in-transition" oninput={onTextareaInput}></textarea> as HTMLTextAreaElement;
 	editableTextElement.addEventListener("focusout", (_) => {
-		setIsFocused(currentContent().length !== 0);
+		if (currentContent().trim().length === 0) {
+			setIsFocused(false);
+			// Ensure that after focus change no whitespace characters are left after
+			setCurrentContent('');
+			editableTextElement.value = '';
+		}
 	});
 
 	return <>
@@ -43,7 +49,7 @@ function TextArea() {
 				{editableTextElement}
 			</Show>
 		</div>
-		<p id="editor-word-count" class="editor-text absolute right-10 bottom-10"><span class="opacity-80">Words:</span> {currentContent().trimEnd().split(' ').length - 1}</p>
+		<p id="editor-word-count" class="editor-text text-center absolute bottom-8"><span class="opacity-60">Words:</span> {wordCount()}</p>
 	</>
 }
 
