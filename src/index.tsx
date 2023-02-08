9edae2aef61074905077e83661bf2d4c5aee5f48
Nikita <mestzatukovky@gmail.com>
2023-02-08 20:39:56 +0700

    Words counter; Better lines UX; Minor improvements

--- src/index.tsx
+++ src/index.tsx
@@ -4,44 +4,61 @@ import {createSignal, For, Show} from "solid-js";
 import "./index.css";
 
 
+// Future TODO: Is it possible to share filestream with Rust? OR! Call Rust code everytime new line is added
+
 function TextAreaEditor() {
 	// How many previous lines is displayed above textarea
 	const MAX_LINES_DISPLAYED = 4;
-	const MAX_LINE_LENGTH = 40;
-	// NOTE: Change that to string[] and add extra one for current line
-	const [currentEditorContent, setCurrentEditorContent] = createSignal("");
-	const [displayedLines, setDisplayedLines] = createSignal<string[]>([]);
+	// Maximum length for single line
+	const MAX_LINE_LENGTH = 50;
+	// Length at which textarea underline color changes to indicate approaching chars limit
+	const WARNING_LINE_LENGTH = MAX_LINE_LENGTH - 8;
+	
+	//
+	let wasCtrlPressedRecently: boolean = false;
+	const [currentEditorLine, setCurrentEditorLine] = createSignal("");
+	const [editorContentLines, setEditorContentLines] = createSignal<string[]>([]);
 	const [isFocused, setIsFocused] = createSignal(false);
 	// Function to format input (aka remove extra whitespaces)
 	const formatInput = (value: string) => value.replace(/\s\s+/g, ' ').trim();
-	const wordCount = () => -1;  // TODO:
+	// Derived signal for total words count
+	// NOTE: This seems super inefficient, but I'll leave it for now and 
+	// test later to see if this actually matters
+	const totalWordsCount = () => {
+		// Format current line
+		let result = formatInput(currentEditorLine()).trimEnd().split(' ').length - 1;
+		// Calculating words from previous lines
+		for (const line of editorContentLines()) {
+			result += line.split(' ').length;
+		}
+		return result;
+	}
 	
-	function AppendLine(line: string) {
-		// Step 0. Format line from textarea
-		const formattedLine = formatInput(line);
+	function AppendLine(textAreaSource: HTMLTextAreaElement) {
+		// Step 1. Format line from textarea
+		const formattedLine = formatInput(textAreaSource.value);
 		if (formattedLine.length === 0) {
 			return;
 		}
-		// Step 1. Update content
-		setCurrentEditorContent(currentEditorContent() + '\n' + formattedLine);
 		// Step 2. Update lines
-		setDisplayedLines(currentEditorContent().split('\n'));
-	}
-	function onTextareaInput(e: InputEvent & {currentTarget: HTMLTextAreaElement; target: Element;}) {
-		if (e.currentTarget.value.length > MAX_LINE_LENGTH) {
-			AppendLine(e.currentTarget.value);
-			e.currentTarget.value = '';
-		}
+		setEditorContentLines([...editorContentLines(), formattedLine]);
+		// Step 3. Clear textarea
+		textAreaSource.value = '';
 	}
 	function onTextareaKeydown(e: KeyboardEvent & { currentTarget: HTMLTextAreaElement; target: Element; }) {
-		// If enter was pressed adding new line
-		if (e.key === "Enter") {
-			AppendLine(e.currentTarget.value);
+		// Pasting text isn't allowed because it can mess up lines
+		if (wasCtrlPressedRecently && e.key === 'v') {
+			e.preventDefault();
+		}
+		// Changing from current line after
+		else if (currentEditorLine().length > MAX_LINE_LENGTH || e.key === "Enter" || (e.key === ' ' && currentEditorLine().length > WARNING_LINE_LENGTH)) {
+			AppendLine(e.currentTarget);
 			e.currentTarget.value = '';
 		}
+		wasCtrlPressedRecently = e.ctrlKey;
 	}
 
-	const placeholderElement = <p class="editor-placeholder">Let your thoughts flow <i>mindlessly</i> like a river...</p> as HTMLParagraphElement;
+	const placeholderElement = <p class="editor-placeholder">Let your thoughts flow <i class="">mindlessly</i> like a river...</p> as HTMLParagraphElement;
 	// When placeholder is clicked it fades away and reveals editable textarea
 	placeholderElement.addEventListener("click", (_) => {
 		const initialAnimValue = placeholderElement.style.animation;
@@ -57,13 +74,18 @@ function TextAreaEditor() {
 
 		placeholderElement.style.animation = fadeOutAnim;
 	});
+	// style - sets width value to fit all characters (for details check out "ch" unit in css)
+	// onInput - updates signal with current line
+	// onkeydown - checks if current line should be "pushed back"
+	const textAreaElement = <textarea rows="1" style={`width: ${MAX_LINE_LENGTH + 1}ch`} onkeydown={onTextareaKeydown} 
+								onInput={e => setCurrentEditorLine(e.currentTarget.value)}
+								class={`editor-text-field fade-in-transition ${currentEditorLine().length >= WARNING_LINE_LENGTH ? "editor-text-field-warning" : ''}`}></textarea> as HTMLTextAreaElement;
 	// When focus from empty textarea is removed placeholder is displayed
-	const textAreaElement = <textarea rows="1" style={`width: ${MAX_LINE_LENGTH + 1}ch`} class="editor-text-field fade-in-transition" onkeydown={onTextareaKeydown} onInput={onTextareaInput}></textarea> as HTMLTextAreaElement;
 	textAreaElement.addEventListener("focusout", (_) => {
-		if (currentEditorContent().trim().length === 0) {
+		if (currentEditorLine().trim().length === 0) {
 			setIsFocused(false);
 			// Ensure that after focus change no whitespace characters are left after
-			setCurrentEditorContent('');
+			setCurrentEditorLine('');
 			textAreaElement.value = '';
 		}
 	});
@@ -71,7 +93,7 @@ function TextAreaEditor() {
 	return <>
 		<div id="editor-text-field-container">
 			<div>
-				<For each={displayedLines().slice(Math.max(displayedLines().length - MAX_LINES_DISPLAYED, 0), displayedLines().length)}>
+				<For each={editorContentLines().slice(Math.max(editorContentLines().length - MAX_LINES_DISPLAYED, 0), editorContentLines().length)}>
 				{(line, i, opacity_delta = Math.floor(100 / (MAX_LINES_DISPLAYED + 1))) => 
 					<span class="editor-text" style={`opacity: ${opacity_delta * (i() + 1)}%`}>{line}<br /></span>
 				}</For>
@@ -80,7 +102,7 @@ function TextAreaEditor() {
 				{textAreaElement}
 			</Show>
 		</div>
-		<p id="editor-word-count" class="editor-text text-center absolute bottom-8"><span class="opacity-60">Words:</span> {wordCount()}</p>
+		<p id="editor-word-count" class="editor-text text-center absolute bottom-8"><span class="opacity-60">Words:</span> {totalWordsCount()}</p>
 	</>
 }
 
